28ff252d92404fcbd7d9d0422433edf6ab71b75a
marcushauge <marcus.hauge1@gmail.com>
2022-03-23 18:47:21 +0100

    Backend now detects rectangles

--- backend/flaskr/app.py
+++ backend/flaskr/app.py
@@ -1,7 +1,40 @@
 from flask import Flask
+import cv2
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/rectangles")
+def detect_rectangles():
+    # Load iamge, grayscale, adaptive threshold
+    image = cv2.imread('d.jpg')
+    result = image.copy()
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,51,9)
+
+    # Fill rectangular contours
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        cv2.drawContours(thresh, [c], -1, (255,255,255), -1)
+
+    # Morph open
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=4)
+
+    # Draw rectangles
+    cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        x,y,w,h = cv2.boundingRect(c)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 3)
+
+    cv2.imshow('thresh', thresh)
+    cv2.imshow('opening', opening)
+    cv2.imshow('image', image)
+    cv2.waitKey()
+    return "<p>Hello!</p>"
